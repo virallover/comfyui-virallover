c526523d16f08722416f08aed03c51d9c3e98b2b
Jeff Ma <mjf429@163.com>
2025-05-22 21:47:57 +0800

    fix

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from .lora_downloader import DownloadAndLoadLoraModelOnly
 from .image_utils.depth_fitter import DepthFitter
-from .image_utils.poisson_fusion import PoissonImageFusion, DebugShape, TensorBatchToImage
 
 
 # 注册到NODE_CLASS_MAPPINGS
@@ -8,17 +7,11 @@ NODE_CLASS_MAPPINGS = {
     # ... 其他节点 ...
     "DownloadAndLoadLoraModelOnly": DownloadAndLoadLoraModelOnly,
     "DepthFitter": DepthFitter,
-    "PoissonImageFusion": PoissonImageFusion,
-    "DebugShape": DebugShape,
-    "TensorBatchToImage": TensorBatchToImage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DownloadAndLoadLoraModelOnly": "Download and Load Lora Model Only",
     "DepthFitter": "Depth Fitter",
-    "PoissonImageFusion": "Poisson Image Fusion",
-    "DebugShape": "Debug Shape",
-    "TensorBatchToImage": "Tensor Batch to Image",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']

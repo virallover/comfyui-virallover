0685c1e7937dec9df9c04776567ed30c56c2a8fe
Jeff Ma <mjf429@163.com>
2025-05-23 18:04:00 +0800

    fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 from .lora_downloader import DownloadAndLoadLoraModelOnly
 from .image_utils.depth_fitter import DepthFitter
-from .image_utils.mask_utils import MaskSubtract
-from .image_utils.cv2_inpaint_fixer import CV2InpaintEdgeFixer
 
 
 # 注册到NODE_CLASS_MAPPINGS
@@ -9,15 +7,11 @@ NODE_CLASS_MAPPINGS = {
     # ... 其他节点 ...
     "DownloadAndLoadLoraModelOnly": DownloadAndLoadLoraModelOnly,
     "DepthFitter": DepthFitter,
-    "MaskSubtract": MaskSubtract,
-    "CV2InpaintEdgeFixer": CV2InpaintEdgeFixer,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DownloadAndLoadLoraModelOnly": "Download and Load Lora Model Only",
     "DepthFitter": "Depth Fitter",
-    "MaskSubtract": "Mask Subtract",
-    "CV2InpaintEdgeFixer": "CV2 Inpaint Edge Fixer",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
